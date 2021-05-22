13db4e8d66e788912befcc261a62b81e707edc93
Aatish Bhatia <aatish@gmail.com>
2021-05-22 00:44:03 -0400

    added chart view

--- vue-definitions.js
+++ vue-definitions.js
@@ -1,10 +1,105 @@
+Vue.component('chart', {
+  
+  props: ['statedata'],
+
+  template: '<div id="chart"></div>',
+
+  mounted() {
+
+    let xmax = this.statedata.map(e => e.positivityrate).reduce((a,b) => a > b ? a : b);
+    xmax = 5*(Math.ceil(100*xmax/5))/100;
+
+    let data = this.statedata.sort((a,b) => parseFloat(b.positivityrate) - parseFloat(a.positivityrate));
+
+    // set the dimensions and margins of the graph
+    var margin = {top: 50, right: 30, bottom: 10, left: 150},
+        width = 500 - margin.left - margin.right,
+        height = 800 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#chart")
+      .append("svg")
+        .attr("viewBox", '0 0 ' + String(width + margin.left + margin.right) + ' ' + String(height + margin.top + margin.bottom))
+      .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+
+      // Add X axis
+      var x = d3.scaleLinear()
+        .domain([0, xmax])
+        .range([ 0, width]);
+      svg.append("g")
+        .attr("transform", "translate(0,0)")
+        .call(d3.axisTop(x).tickFormat(d3.format(".0%")))
+        //.selectAll("text")
+        //  .attr("transform", "translate(-10,0)rotate(-45)")
+        //  .style("text-anchor", "end");
+
+    svg.append("rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", x(0.05))
+            .attr("height", height)
+            .style("fill", 'rgba(0,255,0,0.25)');
+
+      /*
+      svg.append("line")
+         .attr("y1", 0)
+         .attr("y2", height)
+         .attr("x1", x(0.05))
+         .attr("x2", x(0.05))
+         .attr( "stroke", '#378b37' )
+         .attr( "stroke-width", "1" );
+      */
+
+    // Y axis
+    var y = d3.scaleBand()
+      .range([ 0, height ])
+      .domain(this.statedata.map(function(d) { return d.state; }))
+      .padding(1);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+  svg.append("text")
+      .attr("class", "x label")
+      .attr("text-anchor", "middle")
+      .attr("x", width/2)
+      .attr("y", -30)
+      .text("Percentage of tests that are positive");
+
+    // Lines
+    svg.selectAll("myline")
+      .data(data)
+      .enter()
+      .append("line")
+        .attr("x1", function(d) { return x(d.positivityrate); })
+        .attr("x2", x(0))
+        .attr("y1", function(d) { return y(d.state); })
+        .attr("y2", function(d) { return y(d.state); })
+        .attr("stroke", "rgba(0,0,0,0.2)");
+
+    // Circles
+    svg.selectAll("mycircle")
+      .data(data)
+      .enter()
+      .append("circle")
+        .attr("cx", function(d) { return x(d.positivityrate); })
+        .attr("cy", function(d) { return y(d.state); })
+        .attr("r", "4")
+        .style("fill", function(d) { return d.positivityrate > 0.05 ? 'crimson' : '#378b37'; })
+        .attr("stroke", "black");
+
+  },
+
+});
+
 Vue.component('statetable', {
   props: ['statedata'],
   template: `<div>
     <table>
       <tr>
-        <th class="columntitle" @click="key = 'state'"><b>State / Union Territory</b></th>
-        <th class="columntitle" @click="key = 'positivityrate'"><b>Percentage of Positive Tests</b> <br>(1 week average)</th>
+        <th class="columntitle" @click="key = 'state'"><b>State or Union Territory</b></th>
+        <th class="columntitle" @click="key = 'positivityrate'"><b>% Positive Tests</b> <br>(1 week average)</th>
         <th class="columntitle" @click="key = 'change'"><b>Trend</b> <br>(1 week change)</th>
         <th class="columntitle" @click="key = 'weeklytestspercapita'"><b>Weekly Tests</b> <br>(per 1,000 people)</th>
       </tr>
@@ -41,45 +136,13 @@ Vue.component('statetable', {
 
 });
 
-// custom graph component
-Vue.component('graph', {
-
-  props: ['traces', 'layout'],
-
-  template: '<div ref="graph" class="graph"></div>',
-
-  methods: {
-
-    updateGraph() {
-      let traces = JSON.parse(JSON.stringify(this.traces));
-      let layout = JSON.parse(JSON.stringify(this.layout));
-      Plotly.react(this.$refs.graph, traces, layout);
-    },
-
-  },
-
-  mounted() {
-    Plotly.newPlot(this.$refs.graph, [], {}, {responsive: true});
-    if (this.traces) {
-      this.updateGraph();
-    }
-  },
-
-  watch: {
-    traces(val, oldVal) {
-      this.updateGraph();
-    }
-  }
-
-});
-
 // global data
 let app = new Vue({
 
   el: '#root',
 
   mounted() {
-    Plotly.d3.csv('https://raw.githubusercontent.com/aatishb/indiatestpositivitydata/main/statedata.csv', data => {
+    d3.csv('https://raw.githubusercontent.com/aatishb/indiatestpositivitydata/main/statedata.csv', data => {
 
       //this.lastUpdated = data.slice(-1)[0]['Date'];
 
@@ -126,6 +189,7 @@ let app = new Vue({
   },
 
   data: {
+    viewMode: 'table',
     states: ['Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh', 'Dadra and Nagar Haveli and Daman and Diu', 'Delhi', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerala', 'Ladakh', 'Lakshadweep', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal'],
     population: {
       'Andaman and Nicobar Islands': 380581,
